7b3bece4e5e05f7e110cc880ca057468fcc2125a
Sámuel Fekete <feketesamu@gmail.com>
2024-03-02 12:32:57 +0000

    chapter 03

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [TicketsModule],
+  imports: [TicketsModule, PrismaModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
