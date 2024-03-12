35d7a0624693bf97cfa743b4bcd89bc7acbfc570
Sámuel Fekete <feketesamu@gmail.com>
2024-03-12 19:29:54 +0000

    move port to .env

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
