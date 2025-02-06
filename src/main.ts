5596e4a3ae37310a9d87c9b23e77c9f04983c0ec
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-02-06 15:45:21 +0100

    Add Scalar OpenApi docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.BACKEND_PORT);
+  app.use(
+    '/scalar',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
+
+  const port = process.env.BACKEND_PORT || 3300;
+  await app.listen(port);
 }
 bootstrap();
